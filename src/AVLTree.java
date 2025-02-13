6dc17aba7bb0c9c7af23e4928306a3b4d7a68a60
Xunmei Yuan <M3R0NP4N@gmail.com>
2025-02-12 20:16:07 -0500

    git commit okayhopefullythisisright

--- src/AVLTree.java
+++ src/AVLTree.java
@@ -29,13 +29,25 @@ class AVLTree extends BinarySearchTree {
     // Rotate right
     private AVLNode rotateRight(AVLNode y) {
         // TODO: Implement right rotation
-        return null;
+        AVLNode x = (AVLNode) y.left;
+        AVLNode T2 = (AVLNode) x.right;
+        x.right = y;
+        y.left = T2;
+        updateHeight(y);
+        updateHeight(x);
+        return x;
     }
 
     // Rotate left
     private AVLNode rotateLeft(AVLNode x) {
         // TODO: Implement left rotation
-        return null;
+        AVLNode y = (AVLNode) x.right;
+        AVLNode T2 = (AVLNode) y.left;
+        y.left = x;
+        x.right = T2;
+        updateHeight(x);
+        updateHeight(y);
+        return y;
     }
 
     // Insert
@@ -46,7 +58,23 @@ class AVLTree extends BinarySearchTree {
 
     private AVLNode insertRec(AVLNode node, int key) {
         // TODO: Implement AVL insert with balancing
-        return null;
+        if (node == null) return new AVLNode(key);
+        if (key < node.key) node.left = insertRec((AVLNode) node.left, key);
+        else if (key > node.key) node.right = insertRec((AVLNode) node.right, key);
+        else return node;
+        updateHeight(node);
+        int balance = getBalance(node);
+        if (balance > 1 && key < ((AVLNode) node.left).key) return rotateRight(node);
+        if (balance < -1 && key > ((AVLNode) node.right).key) return rotateLeft(node);
+        if (balance > 1 && key > ((AVLNode) node.left).key) {
+            node.left = rotateLeft((AVLNode) node.left);
+            return rotateRight(node);
+        }
+        if (balance < -1 && key < ((AVLNode) node.right).key) {
+            node.right = rotateRight((AVLNode) node.right);
+            return rotateLeft(node);
+        }
+        return node;
     }
 
     // Delete
@@ -57,6 +85,45 @@ class AVLTree extends BinarySearchTree {
 
     private AVLNode deleteRec(AVLNode node, int key) {
         // TODO: Implement AVL delete with balancing
-        return null;
+        if (node == null)
+            return node;
+
+        if (key < node.key) {
+            node.left = deleteRec((AVLNode) node.left, key);
+        } else if (key > node.key) {
+            node.right = deleteRec((AVLNode) node.right, key);
+        } else {
+            if (node.left == null || node.right == null) {
+                AVLNode temp = (node.left != null) ? (AVLNode) node.left : (AVLNode) node.right;
+                if (temp == null) node = null;
+                else node = temp;
+            } else {
+                AVLNode temp = minValueNode((AVLNode) node.right);
+                node.key = temp.key;
+                node.right = deleteRec((AVLNode) node.right, temp.key);
+            }
+        }
+        if (node == null) return node;
+        updateHeight(node);
+        int balance = getBalance(node);
+        if (balance > 1 && getBalance((AVLNode) node.left) >= 0) return rotateRight(node);
+        if (balance > 1 && getBalance((AVLNode) node.left) < 0) {
+            node.left = rotateLeft((AVLNode) node.left);
+            return rotateRight(node);
+        }
+        if (balance < -1 && getBalance((AVLNode) node.right) <= 0) return rotateLeft(node);
+        if (balance < -1 && getBalance((AVLNode) node.right) > 0) {
+            node.right = rotateRight((AVLNode) node.right);
+            return rotateLeft(node);
+        }
+
+        return node;
+    }
+    private AVLNode minValueNode(AVLNode node) {
+        AVLNode current = node;
+        while (current.left != null) {
+            current = (AVLNode) current.left;
+        }
+        return current;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        AVLTree avl = new AVLTree();
+        avl.insert(10);
+        avl.insert(20);
+        avl.insert(30);
+        avl.insert(40);
+        avl.insert(50);
+        avl.insert(25);
+
+        System.out.println("Inorder traversal of the constructed AVL tree is:");
+        avl.inorder();
+
+        avl.delete(40);
+        System.out.println("Inorder traversal after deletion of 40:");
+        avl.inorder();
     }
 }
